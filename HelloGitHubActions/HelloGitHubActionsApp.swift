c5345ec74da1c687e82b17c26332c8d51db10985
Mateus Rodrigues <mateus.fr26@gmail.com>
2023-08-09 09:06:51 -0300

    Initial Commit

---
+++ HelloGitHubActions/HelloGitHubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloGitHubActionsApp.swift
+//  HelloGitHubActions
+//
+//  Created by Mateus on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloGitHubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
